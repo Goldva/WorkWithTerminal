1b49cb2206d4f0549842b31072a3b0d7d097abb3
Goldva <zovode@gmail.com>
2016-11-25 17:52:22 +0400

    Commit

--- src/interfaces/Terminal.java
+++ src/interfaces/Terminal.java
@@ -1,12 +1,10 @@
 package interfaces;
 
-/**
- * Created by ermakov on 25.11.2016.
- */
 public interface Terminal {
     void putMoney(int money);
     void shootMoney(int money);
     void createClient(String name);
     void deleteClient(String name);
+    void createCard();
     void deleteCard();
 }
