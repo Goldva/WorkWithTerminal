eab459d8ea562631ff293b78f96c461cd289aab8
Goldva <zovode@gmail.com>
2016-11-30 17:02:08 +0400

    Добавил сообщение в исключение

--- src/exceptions/SuchUserExistsException.java
+++ src/exceptions/SuchUserExistsException.java
@@ -1,5 +1,7 @@
 package exceptions;
 
 public class SuchUserExistsException extends Exception {
-
+    public SuchUserExistsException() {
+        super("Клиент с таким именем уже существует");
+    }
 }
